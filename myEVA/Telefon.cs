952d2540fd98f63f17ad58f88c48289474e71b53
Maik Hoffmann <maikhoffmann@outlook.com>
2020-09-11 12:17:59 +0200

    erstellung der Datei Telefon.cs struct Telefon

---
+++ myEVA/Telefon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myEVA
+{
+    struct Telefon
+    {
+        public string vorwahl;
+        public int nummer;
+
+        public Telefon(string v, int n)
+        {
+            vorwahl = v;
+            nummer = n;
+        }
+
+        public override string ToString()
+        {
+            return vorwahl + nummer;
+        }
+    }
+}
